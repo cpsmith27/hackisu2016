698191065c4a656b620772d5c29d14a65d5f262b
cpsmith27 <cpsmith@iastate.edu>
2016-09-17 12:13:02 -0500

    Add image processor and color querying.

---
+++ app/src/main/java/com/clarifai/androidstarter/ImageProcessor.java
@@ -0,0 +1,121 @@
+package com.clarifai.androidstarter;
+
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import com.clarifai.api.ClarifaiClient;
+import com.clarifai.api.RecognitionRequest;
+import com.clarifai.api.RecognitionResult;
+import com.clarifai.api.exception.ClarifaiException;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.Random;
+
+/**
+ * Created by chris on 9/17/16.
+ */
+public class ImageProcessor {
+    private byte[] jpeg;
+    private final ClarifaiClient client = new ClarifaiClient(Credentials.CLIENT_ID,
+            Credentials.CLIENT_SECRET);
+
+    ImageProcessor(byte[] img) {
+        this.jpeg = img;
+    }
+    ImageProcessor(Bitmap bitmap) {
+        try {
+            // Scale down the image. This step is optional. However, sending large images over the
+            // network is slow and  does not significantly improve recognition performance.
+            Bitmap scaled = Bitmap.createScaledBitmap(bitmap, 320,
+                    320 * bitmap.getHeight() / bitmap.getWidth(), true);
+
+            // Compress the image as a JPEG.
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            scaled.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            this.jpeg = out.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public RecognitionResult getTags() {
+        RecognitionRequest rq = new RecognitionRequest(jpeg);
+        return client.recognize(rq).get(0);
+    }
+
+    public String getColors() throws IOException {
+        Charset UTF8 = Charset.forName("UTF-8");
+
+        try {
+            // Send request:
+            HttpURLConnection conn;
+            String boundary = Long.toHexString(new Random().nextLong());
+
+            String urlString = "https://api.clarifai.com/v1/color/";
+            URL url = new URL(urlString);
+            conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestProperty("Authorization", "Bearer dYelbmyVAC5UwsMUr4T1AnRGOrFCGc");
+            conn.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+            conn.setDoOutput(true);
+            OutputStream os = conn.getOutputStream();
+            DataOutputStream out = new DataOutputStream(os);
+            try {
+                out.write(("--" + boundary + "\r\n").getBytes(UTF8));
+                String header = "Content-Disposition: form-data;" +
+                        " name=\"" + "encoded_data" + "\";" +
+                        " filename=\"" + "media" + "\"\r\n" +
+                        "Content-Type: application/octet-stream\r\n\r\n";
+                out.write(header.getBytes(UTF8));
+
+                InputStream in = new ByteArrayInputStream(jpeg);
+                byte[] buf = new byte[4096];
+                while (true) {
+                    int numRead = in.read(buf);
+                    if (numRead < 0) {
+                        break;
+                    }
+                    out.write(buf, 0, numRead);
+                }
+                out.write("\r\n".getBytes(UTF8));
+                out.write(("--" + boundary + "--\r\n").getBytes(UTF8));
+                out.flush();
+            } finally {
+                out.close();
+            }
+
+            // Parse result:
+            boolean isSuccess = (conn.getResponseCode() >= 200 && conn.getResponseCode() < 300);
+            if (isSuccess) {
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                return response.toString();
+            } else {
+                return "fail" + conn.getResponseCode();
+            }
+
+        } catch (Exception e) {
+            throw new Error(e.getMessage(), e);
+        }
+    }
+}
--- app/src/main/java/com/clarifai/androidstarter/RecognitionActivity.java
+++ app/src/main/java/com/clarifai/androidstarter/RecognitionActivity.java
@@ -12,43 +12,23 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.clarifai.api.ClarifaiClient;
-import com.clarifai.api.RecognitionRequest;
 import com.clarifai.api.RecognitionResult;
 import com.clarifai.api.Tag;
-import com.clarifai.api.exception.ClarifaiException;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import static android.provider.MediaStore.Images.Media;
-
 
 /**
  * A simple Activity that performs recognition using the Clarifai API.
  */
 public class RecognitionActivity extends Activity {
   private static final String TAG = RecognitionActivity.class.getSimpleName();
-
   private static final int CODE_PICK = 1;
-
-  private final ClarifaiClient client = new ClarifaiClient(Credentials.CLIENT_ID,
-          Credentials.CLIENT_SECRET);
   private Button selectButton;
   private ImageView imageView;
   private TextView textView;
-  /**
-   * ATTENTION: This was auto-generated to implement the App Indexing API.
-   * See https://g.co/AppIndexing/AndroidStudio for more information.
-   */
-  private GoogleApiClient client2;
+  private int mode = 0;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -65,9 +45,6 @@ public class RecognitionActivity extends Activity {
         startActivityForResult(intent, CODE_PICK);
       }
     });
-    // ATTENTION: This was auto-generated to implement the App Indexing API.
-    // See https://g.co/AppIndexing/AndroidStudio for more information.
-    client2 = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
   }
 
   @Override
@@ -76,24 +53,51 @@ public class RecognitionActivity extends Activity {
     if (requestCode == CODE_PICK && resultCode == RESULT_OK) {
       // The user picked an image. Send it to Clarifai for recognition.
       Log.d(TAG, "User picked image: " + intent.getData());
-      Bitmap bitmap = loadBitmapFromUri(intent.getData());
+      final Bitmap bitmap = loadBitmapFromUri(intent.getData());
       if (bitmap != null) {
         imageView.setImageBitmap(bitmap);
         textView.setText("Recognizing...");
         selectButton.setEnabled(false);
 
         // Run recognition on a background thread since it makes a network call.
-        new AsyncTask<Bitmap, Void, RecognitionResult>() {
-          @Override
-          protected RecognitionResult doInBackground(Bitmap... bitmaps) {
-            return recognizeBitmap(bitmaps[0]);
-          }
 
-          @Override
-          protected void onPostExecute(RecognitionResult result) {
-            updateUIForResult(result);
-          }
-        }.execute(bitmap);
+        if (mode == 1) {
+
+          new AsyncTask<Bitmap, Void, String>() {
+            @Override
+            protected String doInBackground(Bitmap... bitmaps) {
+              try {
+                ImageProcessor ip = new ImageProcessor(bitmaps[0]);
+                return ip.getColors();
+              } catch (Exception e) {
+                Log.e("1",e.getMessage());
+                return "fail";
+              }
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+              updateUIForResultString(result);
+            }
+          }.execute(bitmap);
+
+        } else {
+
+          new AsyncTask<Bitmap, Void, RecognitionResult>() {
+            @Override
+            protected RecognitionResult doInBackground(Bitmap... bitmaps) {
+              ImageProcessor ip = new ImageProcessor(bitmaps[0]);
+              return ip.getTags();
+            }
+
+            @Override
+            protected void onPostExecute(RecognitionResult result) {
+              updateUIForResult(result);
+            }
+          }.execute(bitmap);
+
+        }
+
       } else {
         textView.setText("Unable to load selected image.");
       }
@@ -126,35 +130,6 @@ public class RecognitionActivity extends Activity {
   }
 
   /**
-   * Sends the given bitmap to Clarifai for recognition and returns the result.
-   */
-  private RecognitionResult recognizeBitmap(Bitmap bitmap) {
-    try {
-      // Scale down the image. This step is optional. However, sending large images over the
-      // network is slow and  does not significantly improve recognition performance.
-      Bitmap scaled = Bitmap.createScaledBitmap(bitmap, 320,
-              320 * bitmap.getHeight() / bitmap.getWidth(), true);
-
-      // Compress the image as a JPEG.
-      ByteArrayOutputStream out = new ByteArrayOutputStream();
-      scaled.compress(Bitmap.CompressFormat.JPEG, 90, out);
-      byte[] jpeg = out.toByteArray();
-
-      // Send the JPEG to Clarifai and return the result.
-
-      RecognitionRequest rq = new RecognitionRequest(jpeg);
-      getColorRequest("wwww");
-      return client.recognize(rq).get(0);
-    } catch (ClarifaiException e) {
-      Log.e(TAG, "Clarifai error", e);
-      return null;
-    } catch (IOException e) {
-      e.printStackTrace();
-      return null;
-    }
-  }
-
-  /**
    * Updates the UI by displaying tags for the given result.
    */
   private void updateUIForResult(RecognitionResult result) {
@@ -176,19 +151,9 @@ public class RecognitionActivity extends Activity {
     selectButton.setEnabled(true);
   }
 
-  private String getColorRequest(String image_urls) throws IOException {
-    image_urls = "http://449ijl1gws101a1ktoaq0f8kml.wpengine.netdna-cdn.com/wp-content/uploads/2015/07/shutterstock_175728152-795x380.jpg";
-    String access_token = "dYelbmyVAC5UwsMUr4T1AnRGOrFCGc";
-    URL url = new URL("https://api.clarifai.com/v1/color/?access_token=" + access_token + "&url=" + image_urls);
-    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-    try {
-      InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-      Log.e("Error" + in.toString(), "Error");
-      return in.toString();
-    }
-    finally {
-      urlConnection.disconnect();
-    }
+  private void updateUIForResultString(String result) {
+    textView.setText(result);
+    selectButton.setEnabled(true);
   }
 }
 
